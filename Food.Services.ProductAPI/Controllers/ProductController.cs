f1e53e1da643d2acb9c7f9935b20d76993864f9c
Shri Nivas <shri.nivas.contractor@arcelormittal.com>
2024-10-20 15:49:44 +0530

    ProductAPI implementation completed with an error in ProductEdit function Will look into that later

---
+++ Food.Services.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,137 @@
+﻿using AutoMapper;
+using Food.Services.ProductAPI.Data;
+using Food.Services.ProductAPI.Models;
+using Food.Services.ProductAPI.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    [Authorize]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDBContext _db;
+        private readonly ResponseDto _response;
+        IMapper _mapper;
+        public ProductController(ApplicationDBContext applicationDBContext, IMapper mapper)
+        {
+            _db = applicationDBContext;
+            _response = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto GetProducts()
+        {
+            try
+            {
+                IEnumerable<Product> productsList = _db.Products.ToList();
+                _response.Result = _mapper.Map<IEnumerable<ProductDTO>>(productsList);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto GetProduct(int id)
+        {
+
+            try
+            {
+                Product product = _db.Products.FirstOrDefault(x => x.ProductId == id);
+                if (product == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.ErrorMessage = "Product not found";
+                }
+                _response.Result = _mapper.Map<ProductDTO>(product);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = ex.Message;
+
+            }
+
+            return _response;
+
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto AddProduct([FromBody] ProductDTO productDTO)
+        {
+            try
+            {
+                Product product = _mapper.Map<Product>(productDTO);
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<ProductDTO>(product);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = ex.Message;
+            }
+            return _response;
+        }
+        [HttpPut]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto UpdateProduct([FromBody] ProductDTO productDTO)
+        {
+            try
+            {
+                Product product = _mapper.Map<Product>(productDTO);
+                _db.Update(product);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<ProductDTO>(product);
+
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessage = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}") ]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto DeleteProduct(int id)
+        {
+            try
+            {
+                Product product = _db.Products.FirstOrDefault(x => x.ProductId == id);
+                if (product == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.ErrorMessage = "Product not found";
+                }
+                else
+                {
+                    _db.Products.Remove(product);
+                    _db.SaveChanges();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
---
+++ Food.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Food.Web.Models;
+
+namespace Food.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<ResponseDto> GetAllProductAsync();
+        Task<ResponseDto>GetProductByIdAsync(int productId);
+        Task<ResponseDto> CreateProductAsync(ProductDto productDto);
+        Task<ResponseDto> UpdateProductAsync(ProductDto productDto);
+        Task<ResponseDto> DeleteProductAsync(int productId);
+    }
+}
