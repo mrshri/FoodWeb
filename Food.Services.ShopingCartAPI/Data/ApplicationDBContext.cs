84f6b294ac9d9f14a7c3bf1612dcc489e2614602
Shri Nivas <shri.nivas.contractor@arcelormittal.com>
2024-11-17 11:04:39 +0530

    Add ShoppingCartAPI project with configurations and controllers
    Added new project `Food.Services.ShoppingCartAPI` to the solution `FoodWeb.sln`. Updated solution file to include build configurations for the new project. Added configuration files `appsettings.Development.json` and `appsettings.json` with necessary settings. Created HTTP file for API endpoint testing. Added project file with package references for AutoMapper, JWT Bearer Authentication, Entity Framework Core, and Swagger.
    
    Implemented `MappingConfig.cs` for AutoMapper mappings. Updated `Program.cs` to configure services for database connection, JWT authentication, AutoMapper, and Swagger. Created `CartAPIController.cs` for handling API requests. Added `ApplicationDBContext.cs` to define database context. Implemented `WebApplicationBuilderExtensions.cs` for JWT authentication configuration.
    
    Added migration files to create `CartHeaders` and `CartDetails` tables. Updated `ApplicationDBContextModelSnapshot.cs` to reflect current database schema. Created model and DTO classes for shopping cart data. Added `launchSettings.json` for project launch configurations.

---
+++ Food.Services.ShopingCartAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Food.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Food.Services.ShoppingCartAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Food.Services.ShopingCartAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Food.Services.ShoppingCartAPI.Models;
+using Food.Services.ShoppingCartAPI.Models.DTO;
+
+namespace Food.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CartHeader, CartDetailsDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
