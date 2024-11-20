4f9d42aa09661968faeefb86f2f473b6c44fe991
Shri Nivas <shri.nivas.contractor@arcelormittal.com>
2024-11-20 15:57:11 +0530

    Add services and endpoints for product and coupon handling
    CouponAPIController.cs
    - Cleaned up namespaces and added new dependencies in CartAPIController.cs
    - Added new endpoints: GetCart, ApplyCoupon, RemoveCart
    - Fixed CartHeaderId assignment bug in CartUpsert method
    - Added Newtonsoft.Json reference in ShoppingCartAPI.csproj
    - Corrected mapping in MappingConfig.cs for CartHeader to CartHeaderDto
    - Configured services and HTTP clients in Program.cs
    - Updated applicationUrl in launchSettings.json
    - Implemented ICouponService and IProductService
    - Updated appsettings.json with ServiceUrls for ProductAPI and CouponAPI

---
+++ Food.Services.ShopingCartAPI/Service/CouponService.cs
@@ -0,0 +1,29 @@
+﻿using Food.Services.ShoppingCartAPI.Models.DTO;
+using Food.Services.ShoppingCartAPI.Service.IService;
+using Newtonsoft.Json;
+
+namespace Food.Services.ShoppingCartAPI.Service
+{
+    public class CouponService : ICouponService
+    {
+        private IHttpClientFactory _httpClientFactory;
+        public CouponService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<CouponDTO> GetCoupon(string couponCode)
+        {
+            var client = _httpClientFactory.CreateClient("Coupon");
+            var apiResponce = await client.GetAsync($"api/coupon/GetByCode/{couponCode}");
+            var apiContent = await apiResponce.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+            if (response.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<CouponDTO>(Convert.ToString(response.Result));
+            }
+
+            return new CouponDTO();
+
+        }
+    }
+}
---
+++ Food.Services.ShopingCartAPI/Service/IService/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using Food.Services.ShoppingCartAPI.Models.DTO;
+
+namespace Food.Services.ShoppingCartAPI.Service.IService
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+    }
+}
