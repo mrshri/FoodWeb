32b837ff15458254f603733a7c39c05ae8f4f6c1
Shri Nivas <mrshri404@gmail.com>
2024-11-20 16:41:16 +0530

    Add services and endpoints for product and coupon handling (#6)
    CouponAPIController.cs
    - Cleaned up namespaces and added new dependencies in CartAPIController.cs
    - Added new endpoints: GetCart, ApplyCoupon, RemoveCart
    - Fixed CartHeaderId assignment bug in CartUpsert method
    - Added Newtonsoft.Json reference in ShoppingCartAPI.csproj
    - Corrected mapping in MappingConfig.cs for CartHeader to CartHeaderDto
    - Configured services and HTTP clients in Program.cs
    - Updated applicationUrl in launchSettings.json
    - Implemented ICouponService and IProductService
    - Updated appsettings.json with ServiceUrls for ProductAPI and CouponAPI
    
    Co-authored-by: Shri Nivas <shri.nivas.contractor@arcelormittal.com>

--- Food.Services.ShopingCartAPI/Controllers/CartAPIController.cs
+++ Food.Services.ShopingCartAPI/Controllers/CartAPIController.cs
@@ -2,10 +2,9 @@
 using Food.Services.ShoppingCartAPI.Data;
 using Food.Services.ShoppingCartAPI.Models;
 using Food.Services.ShoppingCartAPI.Models.DTO;
-using Microsoft.AspNetCore.Http;
+using Food.Services.ShoppingCartAPI.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection.PortableExecutable;
 
 namespace Food.Services.ShoppingCartAPI.Controllers
 {
@@ -13,15 +12,109 @@ namespace Food.Services.ShoppingCartAPI.Controllers
     [ApiController]
     public class CartAPIController : ControllerBase
     {
-        private readonly IMapper _mapper;
+        private  IMapper _mapper;
         private readonly ApplicationDBContext _db;
-        private readonly ResponseDto _response;
-        public CartAPIController(ApplicationDBContext db, IMapper mapper)
+        private  ResponseDto _response;
+        private  IProductService _productService;
+        private ICouponService _CouponService;
+        public CartAPIController(ApplicationDBContext db, IMapper mapper, IProductService productService, ICouponService couponService)
         {
             _mapper = mapper;
             _db = db;
+            _productService = productService;
+            _CouponService = couponService;
             _response = new ResponseDto();
+            _CouponService = couponService;
         }
+
+
+        [HttpGet("GetCart/{userId}")]
+        public async Task<ResponseDto> GetCart(string userId)
+        {
+            try
+            {
+                CartDto cart = new()
+                {
+                    CartHeader = _mapper.Map<CartHeader>(_db.CartHeaders.First(u => u.UserId == userId)),
+
+                };
+                cart.CartDetails = _mapper.Map<IEnumerable<CartDetailsDto>>(_db.CartDetails
+                   .Where(u => u.CartHeaderId == cart.CartHeader.CartHeaderId));
+
+                IEnumerable<ProductDTO> products = await _productService.GetProducts();
+
+                foreach (var item in cart.CartDetails)
+                {
+                    item.Product = products.FirstOrDefault(u => u.ProductId == item.ProductId);
+                    if (item.Product != null)
+                    {
+                        cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
+                    }
+
+                }
+                //apply coupon if exist
+                if (!string.IsNullOrEmpty(cart.CartHeader.CouponCode))
+                {
+                    CouponDTO coupon = await _CouponService.GetCoupon(cart.CartHeader.CouponCode);
+                    if (coupon != null && cart.CartHeader.CartTotal >= coupon.MinAmount)
+                    {
+                        cart.CartHeader.CartTotal -= coupon.DiscountAmount;
+                        cart.CartHeader.Discount = coupon.DiscountAmount;
+                    }
+                }
+                _response.Result = cart;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpPost("ApplyCoupon")]
+        public async Task<object> ApplyCoupon([FromBody] CartDto cartDto) {
+            try
+            {
+                var cartFromDb = await _db.CartHeaders.FirstAsync(u => u.UserId == cartDto.CartHeader.UserId);
+                cartFromDb.CouponCode = cartDto.CartHeader.CouponCode;
+                _db.CartHeaders.Update(cartFromDb);
+                await _db.SaveChangesAsync();
+                _response.Result = true;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = ex.Message;
+
+            }
+            return _response;
+
+        }
+
+        //[HttpPost("RemoveCoupon")]
+        //public async Task<object> RemoveCoupon([FromBody] CartDto cartDto)
+        //{
+        //    try
+        //    {
+        //        var cartFromDb = await _db.CartHeaders.FirstAsync(u => u.UserId == cartDto.CartHeader.UserId);
+        //        cartFromDb.CouponCode = "";
+        //        _db.CartHeaders.Update(cartFromDb);
+        //        await _db.SaveChangesAsync();
+        //        _response.Result = true;
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _response.IsSuccess = false;
+        //        _response.ErrorMessage = ex.Message;
+
+        //    }
+        //    return _response;
+
+        //}
 
         [HttpPost("CartUpsert")]
         public async Task<ResponseDto> CartUpsert(CartDto cartDto)
@@ -51,7 +144,7 @@ namespace Food.Services.ShoppingCartAPI.Controllers
                     if(cartDetailsFromDb == null)
                     {
                         //create Cartdetails
-                        cartDto.CartDetails.First().CartHeaderId = cartDetailsFromDb.CartHeaderId;
+                        cartDto.CartDetails.First().CartHeaderId = cartHeaderFromDb.CartHeaderId;
                         _db.CartDetails.Add(_mapper.Map<CartDetails>(cartDto.CartDetails.First()));
                         _db.SaveChangesAsync();
                     }
@@ -68,6 +161,31 @@ namespace Food.Services.ShoppingCartAPI.Controllers
 
                 }
                 _response.Result = cartDto;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = e.Message.ToString();
+            }
+            return _response;
+        }
+        [HttpPost("RemoveCart")]
+        public async Task<ResponseDto> RemoveCart([FromBody]int cartDetailsId)
+        {
+            try
+            {
+                CartDetails cartDetails =  _db.CartDetails.First(u => u.CartDetailsId == cartDetailsId);
+                int totalCountOfCart = _db.CartDetails.Where(u => u.CartHeaderId == cartDetails.CartHeaderId).Count();
+                _db.CartDetails.Remove(cartDetails);
+                if (totalCountOfCart == 1) {
+                    var  cartHeaderRemove = await _db.CartHeaders
+                        .FirstOrDefaultAsync(u => u.CartHeaderId == cartDetails.CartHeaderId);
+                    _db.CartHeaders.Remove(cartHeaderRemove);
+
+                }
+                await _db.SaveChangesAsync();
+
+                _response.Result = true;
             }
             catch (Exception e)
             {
---
+++ Food.Services.ShopingCartAPI/Service/IService/ICouponService.cs
@@ -0,0 +1,9 @@
+﻿using Food.Services.ShoppingCartAPI.Models.DTO;
+
+namespace Food.Services.ShoppingCartAPI.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<CouponDTO> GetCoupon(string couponCode);
+    }
+}
